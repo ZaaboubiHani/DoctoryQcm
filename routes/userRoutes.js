463495213a1078c70c89129ba315b026518d66bd
abdou51 <bgh.prods@gmail.com>
2024-09-19 08:12:49 +0100

    fixed update user with token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-const userJwt = require("../middlewares/userJwt");
-const adminJwt = require("../middlewares/adminJwt");
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
+const userJwt = require('../middlewares/userJwt')
+const adminJwt = require('../middlewares/adminJwt')
 // Define routes
 
-router.post("/register", userController.registerUser);
-router.post("/login", userController.loginUser);
-router.put("/:id", userController.updateUser);
-router.get("/me", userJwt, userController.getMe);
-router.get("/", adminJwt,userController.getUsers);
-module.exports = router;
+router.post('/register', userController.registerUser)
+router.post('/login', userController.loginUser)
+router.put('/:id', userJwt, userController.updateUser)
+router.get('/me', userJwt, userController.getMe)
+router.get('/', adminJwt, userController.getUsers)
+module.exports = router
